69c05163f4244f848264bb3105c489f85657b1db
Nuri Buğra Demir <bugranuri.demir@gmail.com>
2023-06-01 22:47:55 +0300

    responsive 1

--- src/js/script.js
+++ src/js/script.js
@@ -32,3 +32,38 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
+
+// Navbar Button (Mobile)
+
+const navBtn = document.querySelector(".nav-btn");
+const nav = document.querySelector("nav");
+const mainNav = document.querySelector(".main-nav");
+const navItems = document.querySelectorAll(".nav-item");
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+
+let showMenu = false;
+
+navBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        navBtn.classList.add("open");
+        nav.classList.add("open");
+        mainNav.classList.add("open");
+        navItems.forEach((item) => item.classList.add("open"));
+        dropdownBtn.forEach((item) => item.classList.add("open"));
+
+        showMenu = true;
+    } else {
+        navBtn.classList.remove("open");
+        nav.classList.remove("open");
+        mainNav.classList.remove("open");
+        navItems.forEach((item) => item.classList.remove("open"));
+        dropdownBtn.forEach((item) => item.classList.remove("open"));
+
+        showMenu = false;
+    }
+}
+
+
+
